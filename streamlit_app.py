f14bdca87468117ecb188a98cc249ee746053d6e
CesarANavarroR <103149096+CesarANavarroR@users.noreply.github.com>
2023-08-30 18:18:02 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,52 +17,51 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+# Let's put a pick list here so they can pick the fruit they want to include 
 
+#Let's put a pick list here so they can pick the fruit they want to include
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
+# Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
 #Create the repeatable code block (called a function)
 def get_fruityvice_data(this_fruit_choice):
-    	fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    	fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    	return fruityvice_normalized
-
-#New Section to display fruitvice api response
+	fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+	fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+	return fruityvice_normalized
+#New Section to display Fruityvice api respoponse
 streamlit.header("Fruityvice Fruit Advice!")
 try:
 	fruit_choice = streamlit.text_input('What fruit would you like information about?')
-  	if not fruit_choice:
-    		streamlit.error("Please select a fruit to get information.")
-  	else:
-    		back_from_function = get_fruityvice_data(fruit_choice)
-    		streamlit.dataframe(back_from_function)
-
+	if not fruit_choice:
+		streamlit.error("Please select a fruit to get information.")
+	else:
+		back_from_function = get_fruityvice_data(fruit_choice)
+		streamlit.dataframe(fruityvice_normalized)
 except URLError as e:
-  	streamlit.error()
+	streamlit.error()
 
 streamlit.header("The fruit load list contains:")
 #Snowflake-related functions
 def get_fruit_load_list():
 	with my_cnx.cursor() as my_cur:
-        	my_cur.execute("select * from fruit_load_list")
-        	return my.cur.fetchall()
-
+	     my_cur.execute("select * from fruit_load_list")
+	     return my_cur.fetchall()
 #Add a button to load the fruit
 if streamlit.button('Get Fruit List'):
-	my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-    	my_data_rows = get_fruit_load_list()
-    	my_cnx.close()
-    	streamlit.dataframe(my_data_rows)
+	my_data_rows = get_fruit_load_list()
+	my_cnx.close()
+	streamlit.dataframe(my_data_rows)
 
-# Allow the end user to add a fruit to the list
+#Allow the end user to add a fruit to the list
 def insert_row_snowflake(new_fruit):
 	with my_cnx.cursor() as my_cur:
-         	my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+		my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 		return "Thanks for adding " + new_fruit
-
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+
 if streamlit.button('Add a Fruit to the List'):
 	my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 	back_from_function = insert_row_snowflake(add_my_fruit)
